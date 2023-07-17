84551528ca7e98712873753d909fbf9edbfbfff2
Victor Rodriguez <victor.r.sanroman@gmail.com>
2023-07-17 20:56:39 +0200

    add seeders

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,8 +21,9 @@ class UserFactory extends Factory
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => '$2y$10$cYM0BgWq9ac74bjwIdKVj.W4LaTHo8vJLewtcUYp6DdAM6kVUYT/.', // password
             'remember_token' => Str::random(10),
+            'trial_ends_at' => now()->addDays(10)
         ];
     }
 
